e5bcd71e0f3344ca749b6079f2ba9f3ec3ca79a1
techynishant <nishantjha1405@gmail.com>
2026-06-03 23:55:29 +0530

    Add collapsible sidebar layout

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
-    $('#dtVerticalScrollExample').DataTable({
-      "scrollY": "200px",
-      "scrollCollapse": true,
-    });
-    $('.dataTables_length').addClass('bs-select');
-  });
+    if ($('#dtVerticalScrollExample').length) {
+        $('#dtVerticalScrollExample').DataTable({
+            scrollY: "200px",
+            scrollCollapse: true
+        });
 
+        $('.dataTables_length').addClass('bs-select');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const sidebarToggleBtn = document.getElementById("sidebarToggleBtn");
+
+    body.classList.add("sidebar-collapsed");
+
+    if (sidebarToggleBtn) {
+        sidebarToggleBtn.addEventListener("click", function () {
+            body.classList.toggle("sidebar-collapsed");
+        });
+    }
+});
